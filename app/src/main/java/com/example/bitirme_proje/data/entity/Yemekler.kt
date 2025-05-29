006831e5093231aac3f7b70975b2e37c6019c4c0
Mualla Şahin <muallashn11@gmail.com>
2025-05-29 03:11:32 +0300

    Bitirme branch'ine proje dosyaları yüklendi

---
+++ app/src/main/java/com/example/bitirme_proje/data/entity/Yemekler.kt
@@ -0,0 +1,12 @@
+package com.example.bitirme_proje.data.entity
+
+import java.io.Serializable
+
+data class Yemekler(
+    val yemek_id: Int,
+    val yemek_adi: String,
+    val yemek_resim_adi: String,
+    val yemek_fiyat: Int
+)
+ : Serializable {
+}
